f0af607b64032c99b9a9293bb11ac8fbea054637
wilkie <wilkie05@gmail.com>
2011-12-28 03:57:46 -0500

    Whitespace!

--- src/engine.cpp
+++ src/engine.cpp
@@ -70,19 +70,19 @@ bool IsoTasty::Engine::_startSDL() {
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,          8);
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,           8);
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,          8);
- 
+
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,          16);
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,         32);
- 
+
     SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,      8);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,    8);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,     8);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,    8);
- 
+
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS,  1);
- 
+
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES,  2);
-	
+
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,        1);
 
 	SDL_Surface* surf_display;
@@ -109,7 +109,7 @@ void IsoTasty::Engine::_fireEvent(void* data) {
 		case SDLK_h:
 			if (event->key.keysym.mod & (KMOD_RSHIFT | KMOD_LSHIFT)) {
 				_view->move(-0.5, 0.0);
-			}
+			}	
 			else {
 				_map->x(_map->x() - 1);
 			}
