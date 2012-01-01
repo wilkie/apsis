5c28408dbdd2707d7f863c0f06ddc284a47dc519
wilkie <wilkie05@gmail.com>
2012-01-01 14:53:41 -0500

    More traditional colors.

--- src/engine.cpp
+++ src/engine.cpp
@@ -10,7 +10,7 @@ IsoTasty::Engine::Engine(VideoSettings* video) {
 
 	_view = new Viewport(video->resolutionX, video->resolutionY);
 	_renderer = new Renderer();
-	newMap(100, 100);
+	newMap(25, 25);
 }
 
 void IsoTasty::Engine::newMap(unsigned int width, unsigned int height) {
