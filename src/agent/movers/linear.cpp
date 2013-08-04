016587523aa1e9e1fac5cae1d2708e590d09446a
wilkie <wilkie05@gmail.com>
2013-08-03 22:53:01 -0400

    Adds Linear mover agent.

---
+++ src/agent/movers/linear.cpp
@@ -0,0 +1,18 @@
+#include "apsis/agent/movers/linear.h"
+
+Apsis::Agent::Movers::Linear::Linear(float direction, float speed)
+  : _speed(speed),
+    _direction(direction),
+    Apsis::Agent::Mover("moves in a line") {
+}
+
+bool Apsis::Agent::Movers::Linear::update(float elapsed,
+                                          std::set<unsigned int>& states,
+                                          const Apsis::Geometry::Rectangle& original,
+                                          Apsis::Geometry::Point& updated) {
+  updated = original.center();
+
+  updated.x += cos(_direction) * (elapsed * _speed);
+  updated.y += sin(_direction) * (elapsed * _speed);
+  return true;
+}
