ab498f9911a67a112208e897ba38d4418477f7b8
wilkie <wilkie05@gmail.com>
2013-08-04 20:14:43 -0400

    Adds documentation to Registry::Event.

--- include/apsis/registry/event.h
+++ include/apsis/registry/event.h
@@ -6,8 +6,18 @@
 
 namespace Apsis {
   namespace Registry {
+    /*
+     *  Contains a registry of all events that exist in the system. Events are
+     *  triggers that occur when conditions are met. For instance, there may
+     *  be an event for when a player character collides with a projectile.
+     *  In this case, the event can be responded to in turn by rule code that
+     *  will subtract health from the player and trigger a knockback state.
+     */
     class Event {
     public:
+      /*
+       *  Registers or returns the existing id for the given event.
+       */
       static unsigned int id(const char* name);
     private:
       static std::vector<std::string> _ids;
