3e071d2435a952ed4af29e06ea4f6576aadf706f
wilkie <wilkie05@gmail.com>
2013-08-04 20:06:01 -0400

    Adds documentation to Registry::State.

--- include/apsis/registry/state.h
+++ include/apsis/registry/state.h
@@ -6,8 +6,19 @@
 
 namespace Apsis {
   namespace Registry {
+    /*
+     *  Contains a registry of all states that exist in the system. States are
+     *  context for an object in the system. For instance, a player character
+     *  may have a "jumping" state that is active when the character is within
+     *  a jump. States can be queried on an object and changes in state can be
+     *  responded to through animates, applying new states, allowing certain
+     *  actions, etc.
+     */
     class State {
     public:
+      /*
+       *  Registers or returns the existing id for the given state.
+       */
       static unsigned int id(const char* name);
     private:
       static std::vector<std::string> _ids;
