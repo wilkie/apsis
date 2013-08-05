1f4c8c4d9688d3f8da5924cd8299a1ca07ceedab
wilkie <wilkie05@gmail.com>
2013-08-04 20:14:59 -0400

    Adds documentation to Registry::Property.

--- include/apsis/registry/property.h
+++ include/apsis/registry/property.h
@@ -6,8 +6,18 @@
 
 namespace Apsis {
   namespace Registry {
+    /*
+     *  Contains a registry of all properties in the system. A property is a
+     *  key of which an object can have a value. For instance, a common
+     *  property for an object to have would be "hitPoints" which contains the
+     *  health of the actor. Events can trigger rules that will then subtract
+     *  or add to this value for an object.
+     */
     class Property {
     public:
+      /*
+       *  Registers or returns the existing id for the given property.
+       */
       static unsigned int id(const char* name);
     private:
       static std::vector<std::string> _ids;
