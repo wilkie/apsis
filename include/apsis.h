698fef3011ea08462d2e62872aabcc61b7db4b80
wilkie <wilkie05@gmail.com>
2013-09-28 19:39:58 -0400

    Adds namespace documentation.

--- include/apsis.h
+++ include/apsis.h
@@ -1,4 +1,87 @@
 /*  Apsis -- OpenSource Multipurpose Game Engine  */
 
-#include "apsis/world/terrain.h"
-#include "apsis/world/tile.h"
+/*
+ *  This is the main namespace for Apsis. All public code is available through
+ *  this namespace.
+ */
+namespace Apsis {
+  /*
+   *  Contains various classes that are responsible for managing subsystems.
+   */
+  namespace Engine {
+  }
+
+  /*
+   *  Contains classes used to describe the environment that interacting
+   *  objects in the system live.
+   */
+  namespace World {
+  }
+
+  /*
+   *  Contains low-level classes that help synchronize and time events occuring
+   *  throughout the system.
+   */
+  namespace Sync {
+  }
+
+  /*
+   *  Contains configuration classes that describe what types of features and
+   *  what properties of those features are used.
+   */
+  namespace Settings {
+  }
+
+  /*
+   *  Contains collections of systems objects as they are loaded into the
+   *  system and assigns each a unique identifier.
+   */
+  namespace Registry {
+  }
+
+  /*
+   *  Contains low-level graphics primitives used through the system to build
+   *  more complicated objects.
+   */
+  namespace Primitives {
+  }
+
+  /*
+   *  Contains the classes related to describing and dealing with basic
+   *  geometry.
+   */
+  namespace Geometry {
+  }
+
+  /*
+   *  Contains classes related to the human/user interface, such as widgets
+   *  (buttons, check boxes, etc) and behaviors related to handling input.
+   */
+  namespace Interface {
+  }
+
+  /*
+   *  Contains classes related to three-dimensional objects and rendering.
+   */
+  namespace Model {
+  }
+
+  /*
+   *  Contains classes related to two-dimensional objects and rendering.
+   */
+  namespace Sprite {
+  }
+
+  /*
+   *  Contains classes related to the playing and manipulation of audio.
+   */
+  namespace Audio {
+  }
+
+  /*
+   *  Contains classes related to low-level support for various devices
+   *  and capabilities.
+   */
+  namespace Backend {
+  }
+}
