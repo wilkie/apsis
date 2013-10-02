143da0c9a6e87fd1e26b416185e72af3a89ef91b
wilkie <wilkie05@gmail.com>
2013-10-02 17:10:18 -0400

    Adds Linux implementation of Clock.

--- src/sync/clock.cpp
+++ src/sync/clock.cpp
@@ -6,32 +6,56 @@
   #include <sys/time.h>
 #endif
 
+#include <string.h>
+
 namespace Apsis {
   struct ClockPlatformInternal {
 #ifdef _WIN32
     LARGE_INTEGER _freq, _start, _end;
 #elif defined(__linux)
+    timeval _now;
+    timeval _last;
 #endif
   };
 }
 
 Apsis::Clock::Clock() {
-#ifdef _WIN32
   ClockPlatformInternal* clock = new ClockPlatformInternal;
   _internal = (void*)clock;
+#ifdef _WIN32
   QueryPerformanceFrequency(&clock->_freq);
   QueryPerformanceCounter(&clock->_start);
 #elif defined(__linux)
+  gettimeofday(&clock->_now, NULL);
 #endif
 }
 
+#ifdef __linux
+static float _gettimeofday_difference(timeval *b, timeval *a) {
+  timeval diff_tv;
+
+  diff_tv.tv_usec = b->tv_usec - a->tv_usec;
+  diff_tv.tv_sec = b->tv_sec - a->tv_sec;
+
+  if (a->tv_usec > b->tv_usec) {
+    diff_tv.tv_usec += 1000000;
+    diff_tv.tv_sec--;
+  }
+
+  return (float)diff_tv.tv_sec + ((float)diff_tv.tv_usec / 1000000.0f);
+}
+#endif
+
 float Apsis::Clock::elapsedTime() {
-#ifdef _WIN32
   ClockPlatformInternal* clock = (ClockPlatformInternal*)_internal;
+#ifdef _WIN32
   QueryPerformanceCounter(&clock->_end);
   LONGLONG amt = (clock->_end.QuadPart - clock->_start.QuadPart) * 1000 / clock->_freq.QuadPart;
   clock->_start = clock->_end;
   return (float)amt / 1000.0f;
 #elif defined(__linux)
+  clock->_last = clock->_now;
+  gettimeofday(&clock->_now, NULL);
+  return _gettimeofday_difference(&clock->_now, &clock->_last);
 #endif
 }
