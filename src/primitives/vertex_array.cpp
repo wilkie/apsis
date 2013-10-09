b4e7b62454bc55413a309ff1cb8bf68dfddae512
wilkie <wilkie05@gmail.com>
2013-10-09 02:39:52 -0400

    Adds our own gluErrorString implementation.

--- src/primitives/vertex_array.cpp
+++ src/primitives/vertex_array.cpp
@@ -19,7 +19,20 @@ static void _throwError(const char* function, const char* message) {
 static void _throwGLError(const char* function) {
   GLenum error = glGetError();
   if (error != GL_NO_ERROR) {
-    _throwError(function, (char*)gluErrorString(error));
+    const char* errorString;
+    switch ( error ) {
+      case GL_INVALID_ENUM: errorString = "invalid enumerant"; break;
+      case GL_INVALID_VALUE: errorString = "invalid value"; break;
+      case GL_INVALID_OPERATION: errorString = "invalid operation"; break;
+      case GL_STACK_OVERFLOW: errorString = "stack overflow"; break;
+      case GL_STACK_UNDERFLOW: errorString = "stack underflow"; break;
+      case GL_OUT_OF_MEMORY: errorString = "out of memory"; break;
+      case GL_TABLE_TOO_LARGE: errorString = "table too large"; break;
+      case GL_INVALID_FRAMEBUFFER_OPERATION: errorString = "invalid framebuffer operation"; break;
+//      case GL_TEXTURE_TOO_LARGE: errorString = "texture too large"; break;
+      default: errorString = "unknown GL error"; break;
+    }
+    _throwError(function, errorString);
   }
 }
 #endif
